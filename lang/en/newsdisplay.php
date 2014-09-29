58dd4acdbcbdcf0814e9159c98fa21664f4e55ea
Elizabeth Dalton <elizabeth.dalton@granite.edu>
2014-09-29 11:02:53 -0400

    initial project version

---
+++ lang/en/newsdisplay.php
@@ -0,0 +1,40 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for newsdisplay
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_newsdisplay
+ * @copyright  2011 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'newsdisplay';
+$string['modulenameplural'] = 'newsdisplays';
+$string['modulename_help'] = 'Use the newsdisplay module to display the most recent post(s) to the Course News forum.';
+$string['newsdisplayfieldset'] = 'Custom example fieldset';
+$string['newsdisplayname'] = 'newsdisplay name';
+$string['newsdisplayname_help'] = 'This is the content of the help tooltip associated with the newsdisplayname field. Markdown syntax is supported.';
+$string['newsdisplay'] = 'newsdisplay';
+$string['pluginadministration'] = 'newsdisplay administration';
+$string['pluginname'] = 'newsdisplay';
